03a1b9976c01530e8b5beda100048bfbdcc8dfdd
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-06 12:19:20 -0400

    Complete chapter 3

---
+++ 03.variables/exercises.rb
@@ -0,0 +1,17 @@
+myString = "...you can say that again..." #strings must start with lc letter
+puts myString
+puts myString
+
+composer = "Mozart"
+puts "I prefer " + composer + ", personally."
+composer = "Beethoven"
+puts "Me, I like " + composer
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts "===="
+var1 = "eight"
+puts var1
+puts var2
