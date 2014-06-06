a47db1ea9ab46c8b1b4e0ab5f66e526a05a3df85
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-06-06 12:05:50 -0400

    Complete chapter 1

---
+++ 01.numbers/exercises.rb
@@ -0,0 +1,11 @@
+# hours in a year
+puts 365 * 24
+
+# minutes in a decade
+puts 10 * 365 * 24 * 60
+
+# my age in seconds
+puts 26 * 365 * 24 * 60 * 60
+
+# 1031 million seconds in years
+puts 1031000000.0 / (60 * 24 * 365)
